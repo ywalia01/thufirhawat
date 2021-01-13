b4147854c19d198b7fc37d6b3716e365172dbe43
Yash Walia <yashwalia@Yashs-MacBook-Air.local>
2021-01-13 15:30:12 +0530

    enhance vocabulary

--- bot.rb
+++ bot.rb
@@ -8,13 +8,35 @@ bot.get_updates(fail_silently: true) do |message|
 
   message.reply do |reply|
     case command
-    when /start/i
-      reply.text = "All I can do is say hello. Try the /greet command."
+    when /list/i
+      reply.text = "[/tmkc], [/govt], [/wd], [/list], [/heavy], [/cong], [/greet], [/swear], [/rfor], [/sarthak], [/janit], [/manas], [/yash], [/sleep]"
+    when /tmkc/i
+        reply.text = "तेरी मा की चूत"
+    when /govt/i
+        reply.text = "Ye bik gayi hai gormint, ab gormint mein kuch nahi. Ye behenchod saare milke humko pagal bana rahe hain madarchod ke bacche. Itne gaandphati ke hain ye bhadve."
+    when /wd/i
+        reply.text = "Waell Done!"
+    when /heavy/i
+        reply.text = "Tam toh bade heavy driver hoe"
+    when /cong/i
+        reply.text = "Congrates bhai!"
+    when /rfor/i
+      reply.text = "Randi @srivsarthak"
     when /swear/i
-        reply.text = "Abey jaa na bhosdike."
+        reply.text = "Abey jaa na bhosdike"
+    when /sarthak/i
+        reply.text = "Abey jaldi aaja bhen ke lode @srivsarthak"
+    when /janit/i
+        reply.text = "Abey jaldi aaja bhen ke lode @janitchawla"
+    when /manas/i
+        reply.text = "Abey jaldi aaja bhen ke lode @manas502"
+    when /yash/i
+        reply.text = "Abey jaldi aaja bhen ke lode @ywalia01"
+    when /sleep/i
+        reply.text = "Abey so ke toh dekh bkl @srivsarthak"
     when /greet/i
       greetings = ['bonjour', 'hola', 'hallo', 'sveiki', 'namaste', 'salaam', 'szia', 'halo', 'ciao']
-      reply.text = "#{greetings.sample.capitalize}, #{message.from.first_name}!"
+      reply.text = "#{greetings.sample.capitalize} #{"bsdk"}, #{message.from.first_name}!"
     else
       reply.text = "I have no idea what #{command.inspect} means."
     end
